3573290d96b39be1aae7d18b0303eb98d31638a2
theirix <theirix@gmail.com>
2013-02-17 06:49:26 +0400

    Version 0.0.4 bump

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_pdf_wiki do
   name 'WikiToPdf plugin'
   author 'Arnaud Martel'
   description 'Export wiki pages to PDF file'
-  version '0.0.3'
+  version '0.0.4'
   
   # due to Dispatcher changes
   requires_redmine :version_or_higher => '2.0.0'
